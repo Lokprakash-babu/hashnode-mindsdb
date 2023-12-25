fddc8a67722df84b07dacb88796f8d5730b9bbe0
Lokprakash-babu <lokprakash100@gmail.com>
2023-12-25 11:26:33 +0530

    summary

--- app/learn/[id]/ActionsContainer.tsx
+++ app/learn/[id]/ActionsContainer.tsx
@@ -1,3 +1,5 @@
+import Toast from "@/app/Components/Toasts/Toast";
+import { requestWrapper } from "@/lib/requestWrapper";
 import {
   Modal,
   ModalContent,
@@ -6,47 +8,78 @@ import {
   ModalFooter,
   Button,
   useDisclosure,
+  Spinner,
 } from "@nextui-org/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 
 const Summarize = ({ content }) => {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const [httpRequest, setHttpRequest] = useState({
+    data: "",
+    loading: true,
+    error: "",
+  });
+  useEffect(() => {
+    if (isOpen) {
+      setHttpRequest({
+        data: "",
+        loading: true,
+        error: "",
+      });
+      requestWrapper("/summary", {
+        method: "POST",
+        body: JSON.stringify({
+          content,
+        }),
+      })
+        .then((response) => {
+          setHttpRequest({
+            data: response.message,
+            loading: false,
+            error: "",
+          });
+        })
+        .catch((err) => {
+          setHttpRequest({
+            data: "",
+            loading: false,
+            error: err,
+          });
+        });
+    }
+  }, [isOpen]);
 
   return (
     <>
       <Button onClick={onOpen}>Summarize</Button>
       <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
         <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader className="flex flex-col gap-1">Summary</ModalHeader>
-              <ModalBody>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Nullam pulvinar risus non risus hendrerit venenatis.
-                  Pellentesque sit amet hendrerit risus, sed porttitor quam.
-                </p>
-                <p>
-                  Magna exercitation reprehenderit magna aute tempor cupidatat
-                  consequat elit dolor adipisicing. Mollit dolor eiusmod sunt ex
-                  incididunt cillum quis. Velit duis sit officia eiusmod Lorem
-                  aliqua enim laboris do dolor eiusmod. Et mollit incididunt
-                  nisi consectetur esse laborum eiusmod pariatur proident Lorem
-                  eiusmod et. Culpa deserunt nostrud ad veniam.
-                </p>
-              </ModalBody>
-              <ModalFooter>
-                <Button color="danger" variant="light" onPress={onClose}>
-                  Close
-                </Button>
-              </ModalFooter>
-            </>
-          )}
+          {(onClose) => {
+            return (
+              <>
+                <ModalHeader className="flex flex-col gap-1">
+                  Summary
+                </ModalHeader>
+                <ModalBody>
+                  {httpRequest.loading && <Spinner />}
+                  {httpRequest.loading === false && !!httpRequest.data && (
+                    <p>{httpRequest.data + "..."}</p>
+                  )}
+                  {httpRequest.loading === false && !!httpRequest.error && (
+                    <p>
+                      Something went wrong! Please close and reopen the dialog
+                    </p>
+                  )}
+                </ModalBody>
+                <ModalFooter>
+                  <Button color="danger" variant="light" onPress={onClose}>
+                    Close
+                  </Button>
+                </ModalFooter>
+              </>
+            );
+          }}
         </ModalContent>
       </Modal>
     </>
