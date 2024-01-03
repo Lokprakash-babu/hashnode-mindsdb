142c7fc9f641491988cfdac3039f92d849068df0
Lokprakash-babu <lokprakash100@gmail.com>
2024-01-03 23:05:07 +0530

    Submission listing

--- app/api/submissions/route.tsx
+++ app/api/submissions/route.tsx
@@ -1,13 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
 import { mysqlConnection } from "@/lib/mysql-connection";
-import { currentUser } from "@clerk/nextjs";
+import { auth, currentUser } from "@clerk/nextjs";
 
 //TODO: Add user id to the query
-const getUsersSubmission = () => {
+const getUsersSubmission = (candidate_id, type) => {
   return `
-  SELECT ${process.env.NEXT_PLANETSCALE_DB_NAME}.Submission.id, ${process.env.NEXT_PLANETSCALE_DB_NAME}.Submission.entity_id, ${process.env.NEXT_PLANETSCALE_DB_NAME}.Feedback.score, ${process.env.NEXT_PLANETSCALE_DB_NAME}.Feedback.feedback
-  FROM  ${process.env.NEXT_PLANETSCALE_DB_NAME}.Submission
-  JOIN ${process.env.NEXT_PLANETSCALE_DB_NAME}.Feedback ON ${process.env.NEXT_PLANETSCALE_DB_NAME}.Submission.id = ${process.env.NEXT_PLANETSCALE_DB_NAME}.Feedback.submission_id;
+  SELECT 
+  ${process.env.NEXT_PLANETSCALE_DB_NAME}.Submission.id, 
+  ${process.env.NEXT_PLANETSCALE_DB_NAME}.Submission.entity_id, 
+  ${process.env.NEXT_PLANETSCALE_DB_NAME}.Feedback.score, 
+  ${process.env.NEXT_PLANETSCALE_DB_NAME}.Feedback.feedback
+FROM  
+  ${process.env.NEXT_PLANETSCALE_DB_NAME}.Submission
+JOIN 
+  ${process.env.NEXT_PLANETSCALE_DB_NAME}.Feedback 
+ON 
+  ${process.env.NEXT_PLANETSCALE_DB_NAME}.Submission.id = ${process.env.NEXT_PLANETSCALE_DB_NAME}.Feedback.submission_id
+WHERE 
+  ${process.env.NEXT_PLANETSCALE_DB_NAME}.Submission.candidate_id = "${candidate_id}" 
+  AND ${process.env.NEXT_PLANETSCALE_DB_NAME}.Submission.type = "${type}";
     `;
 };
 export async function GET(req: NextRequest) {
@@ -18,17 +29,22 @@ export async function GET(req: NextRequest) {
     //TODO: Get the user Id from cookie/headers
     // const userId =
     const mysql = await mysqlConnection();
+    const { userId } = auth();
+
     const searchParams = req.nextUrl.searchParams;
     const type = searchParams.get("type") || "contest";
     console.log("type", type);
 
-    const [submissionList] = await mysql.query(getUsersSubmission());
+    const [submissionList] = await mysql.query(
+      getUsersSubmission(userId || "", type)
+    );
     console.log(submissionList);
 
     return NextResponse.json({
       message: submissionList,
     });
   } catch (err) {
+    console.log("error in submission route", err);
     return new NextResponse("Internal error", { status: 500 });
   }
 }
---
+++ app/submissions/SubmissionListingTabs.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { Tab, Tabs } from "@nextui-org/react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import SubmissionList from "./SubmissionListing";
+
+export default function SubmissionListingTab() {
+  const searchParam = useSearchParams();
+  const pageType = searchParam.get("type");
+  const router = useRouter();
+  const pathname = usePathname();
+  const [selected, setSelected] = useState("Practice");
+
+  useEffect(() => {
+    console.log("page type", {
+      pathname,
+      pageType,
+    });
+    if (!pageType || (pageType !== "practice" && pageType !== "contest")) {
+      router.push(`${pathname}?type=practice`);
+    }
+  }, [pathname]);
+  return (
+    <Tabs
+      aria-label="Submission Options"
+      selectedKey={selected}
+      //@ts-ignore
+      onSelectionChange={(value) => {
+        //@ts-ignore
+        setSelected(value);
+        router.push(`${pathname}?type=${value}`);
+      }}
+      className="mt-8"
+    >
+      <Tab key="practice" title="Practice">
+        <SubmissionList type={"practice"} />
+      </Tab>
+      <Tab key="contest" title="Contest">
+        <SubmissionList type={"contest"} />
+      </Tab>
+    </Tabs>
+  );
+}
