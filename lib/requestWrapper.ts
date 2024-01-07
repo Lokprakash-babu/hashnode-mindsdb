730cb7ebb6a1b1143eb27f8d48b84c6faf44c8c4
Lokprakash-babu <lokprakash100@gmail.com>
2024-01-07 11:20:42 +0530

    host removed

--- lib/requestWrapper.ts
+++ lib/requestWrapper.ts
@@ -1,10 +1,7 @@
 export const requestWrapper = async (url: string, options = {}) => {
   try {
-    console.log(`${process.env.NEXT_PUBLIC_HOST}/api/${url}`);
-    const data = await fetch(
-      `${process.env.NEXT_PUBLIC_HOST}/api/${url}`,
-      options
-    );
+    console.log(`/api/${url}`);
+    const data = await fetch(`/api/${url}`, options);
     return await data.json();
   } catch (err) {
     console.error(err);
